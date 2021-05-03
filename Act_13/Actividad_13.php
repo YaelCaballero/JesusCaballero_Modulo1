3a93dddf605c62290129609052623432398cdb6e
Niunk <gerechtigkeit@comunidad.unam.mx>
2021-05-02 22:52:58 -0500

    FEAT historial de disparos

--- Act_13/Actividad_13.php
+++ Act_13/Actividad_13.php
@@ -9,11 +9,29 @@
 <body>
     <table border="1">
     <?php
-        if(isset($_POST["pos_x"]) && isset($_POST["pos_y"]))
+        $jugadas = isset($_POST["veces"])?$_POST["veces"]+1:0;
+        $i=1;
+        $nom2="cordes".$i;
+        if(isset($_POST["$nom2"]))
         {
+            while(isset($_POST["$nom2"]))
+            {
+                $arr_coords[$i] = $_POST["$nom2"];
+                $i++;
+                $nom2 = "cordes".$i;
+            }
+        }    
+        if(isset($_POST["coordX"]) && isset($_POST["coordY"]) && $jugadas > 0)
+        {
+            $coordX = $_POST["coordX"];
+            $coordY = $_POST["coordY"];
+            $coord = $coordX.$coordY;
+       //     array_push($arr_coords, $coord);
+            $arr_coords[$jugadas] = $coord;
             $vidas = $_POST["vida"];
             $vidas--;
-        }
+            var_dump($arr_coords);
+        }  
         else
         {
             $vidas = 10;
@@ -26,7 +44,18 @@
         {
          echo "<th><img src='.\corazon.png' width='70' height='70'</th>";
         } 
-        echo "</tr></table><br><br><table>"; 
+        echo "</tr></table><br><br>"; 
+        echo "Historial de disparos:<br><br>";
+        if($jugadas >= 1)
+        {
+            foreach($arr_coords as $key => $value)
+            {
+                echo $value;
+                if($jugadas > 1)
+                    echo ", ";
+            }
+        }
+        echo "<table>";
         for($i=1; $i<=$z; $i++)
          {
                 echo "<tr>";
@@ -39,10 +68,20 @@
         }
         echo "</table><br><br>";
         echo "<form action='Actividad_13.php' method='post'>
-            Posición x (Letra)<input type='text' name='pos_x' required>
-            Posición y (Letra)<input type='text' name='pos_y' required>
+            Posición x (Letra)<input type='text' name='coordX' required>
+            Posición y (Número)<input type='text' name='coordY' required>
             <input type='submit' value='Disparar'>
-            <input type='hidden' value='$vidas' name='vida'></form>";
+            <input type='hidden' value='$vidas' name='vida'>
+            <input type='hidden' value='$jugadas' name='veces'>";
+            if($jugadas >= 1)
+            {
+                for($i=1;$i<=count($arr_coords);$i++)
+                {
+                    $nom = "cordes".$i;
+                    echo "<input type='hidden' value='$arr_coords[$i]' name='$nom'>";
+                }
+            }
+            echo "</form>";
     ?>
     </table>
 </body>
