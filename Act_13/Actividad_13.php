bc6bf27bbf2aee21cb83f3290c00a7fed138afc3
Yaelo <yaelcaballero504@gmail.com>
2021-05-02 20:11:55 -0500

    FEAT variables

--- Act_13/Actividad_13.php
+++ Act_13/Actividad_13.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Batalla naval</title>
 </head>
 <body>
+    <form action="Actividad_13.php" method="POST">
+        <label>
+            <input type="text" name="coordX" placeholder="A-J">
+        </label>
+        <label>
+            <input type="text" name="coordY" placeholder="1-10">
+        </label>
+        <input type="submit" value="disparar">
+    </form>
     <table border="1">
     <?php
+        $coordX = (isset($_POST["coordX"]) && $_POST["coordX"] !="") ?$_POST["coordX"] : "No especificó";
+        $coordY = (isset($_POST["coordY"]) && $_POST["coordY"] !="") ?$_POST["coordY"] : "No especificó";
+        $coordX = strtoupper($coordX);
+        $valoresX = ["A","B","C","D","E","F","G","H","I","J"];
+        $valoresY = ["1","2","3","4","5","6","7","8","9","10"];
         $z = 10;
         $mar = "<img src=\"mar.jpg\" width=\"50\" height=\"50\"";
+        $noBarco = "<img src=\"noBarco.jpg\" width=\"50\" height=\"50\"";
+        $barco = "<img src=\"Barco.jpg\" width=\"50\" height=\"50\"";
         for($i=0; $i<=$z; $i++) {
                 echo "<tr>";
             for($j=0; $j<=$z; $j++) {
