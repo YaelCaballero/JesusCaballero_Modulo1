ac26f243ed461c8b80227fb4cc33f1286cc6a1b8
Niunk <gerechtigkeit@comunidad.unam.mx>
2021-05-02 20:31:56 -0500

    FEAT: vidas

--- Act_13/Actividad_13.php
+++ Act_13/Actividad_13.php
@@ -9,9 +9,26 @@
 <body>
     <table border="1">
     <?php
+        if(isset($_POST["pos_x"]) && isset($_POST["pos_y"]))
+        {
+            $vidas = $_POST["vida"];
+            $vidas--;
+        }
+        else
+        {
+            $vidas = 10;
+        }
         $z = 11;
-        $mar = "<img src=\"mar.jpg\" width=\"50\" height=\"50\"";
-        for($i=1; $i<=$z; $i++) {
+        $mar = "<img src='.\mar.jpg' width='50' height='50\'";
+        echo "<h1>Batalla naval</h1><br><br>";
+        echo "<h3>Vidas:</h3><table><tr>";
+        for($i=0; $i<$vidas; $i++)    
+        {
+         echo "<th><img src='.\corazon.png' width='70' height='70'</th>";
+        } 
+        echo "</tr></table><br><br><table>"; 
+        for($i=1; $i<=$z; $i++)
+         {
                 echo "<tr>";
             for($j=1; $j<=$z; $j++) {
                 echo "<td>";
@@ -20,6 +37,12 @@
             }
             echo "</tr>";
         }
+        echo "</table><br><br>";
+        echo "<form action='Actividad_13.php' method='post'>
+            Posición x (Letra)<input type='text' name='pos_x' required>
+            Posición y (Letra)<input type='text' name='pos_y' required>
+            <input type='submit' value='Disparar'>
+            <input type='hidden' value='$vidas' name='vida'></form>";
     ?>
     </table>
 </body>
